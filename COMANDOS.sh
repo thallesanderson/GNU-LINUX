1841a87db6d13c75f3cc04fc2cd4607d8f84612e
Thalles Anderson <thallesanderson@hotmail.com>
2021-07-13 19:20:49 -0300

    Habilitar OPEN VPN no GNOME
    ABILITAR O SERVIÇO OPEN VPN NO AMBIENTE GNOME

--- COMANDOS.sh
+++ COMANDOS.sh
@@ -3,11 +3,16 @@
 1 - HABILITAR O PAREAMENTO DO JOYSTICK (XBOX ONE) PARA USO VIA BLUETOOTH.
 
 sudo apt install exonerate ; echo 1 > /sys/module/bluetooth/parameters/disable_ertm ; echo 'options bluetooth disable_ertm=Y' | sudo tee -a /etc/modprobe.d/bluetooth.conf
-
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 2 - INSTALAÇÃO DE DRIVERS DAS PLACAS DE VIDEO "AMD".
 
 sudo apt-get install firmware-amd-graphics
-
-3 - INSTALAÇÃO DOS DRIVERS DAS NOVAS PLACAS DE WIFI 6 & BLUETOOTH 5 DA INTEL.
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+3 - INSTALAÇÃO DOS DRIVERS DAS NOVAS PLACAS DE "WIFI 6 & BLUETOOTH 5" DA INTEL.
 
 sudo apt-get install firmware-iwlwifi
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+04 - HABILITAR O SERVIÇO OPEN VPN NO AMBIENTE GNOME.
+
+sudo apt-get install -y openvpn network-manager-openvpn network-manager-openvpn-gnome
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
