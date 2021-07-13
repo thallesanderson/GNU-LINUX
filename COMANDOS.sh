4cd4321ab42cfeae8710bb7f65b0705184d1b3f6
Thalles Anderson <thallesanderson@hotmail.com>
2021-07-13 19:15:04 -0300

    DRIVERS AMD
    INSTALAÇÃO DE DRIVERS DAS PLACAS DE VIDEO "AMD"

--- COMANDOS.sh
+++ COMANDOS.sh
@@ -3,3 +3,8 @@
 1 - HABILITAR O PAREAMENTO DO JOYSTICK (XBOX ONE) PARA USO VIA BLUETOOTH.
 
 sudo apt install exonerate ; echo 1 > /sys/module/bluetooth/parameters/disable_ertm ; echo 'options bluetooth disable_ertm=Y' | sudo tee -a /etc/modprobe.d/bluetooth.conf
+
+2 - INSTALAÇÃO DE DRIVERS DAS PLACAS DE VIDEO "AMD".
+
+sudo apt-get install firmware-amd-graphics
+
