e175c1991bbaedcf35a6f9e64206cb2095a13423
Thalles Anderson <thallesanderson@hotmail.com>
2021-07-13 19:30:12 -0300

    HABILITAR CANAL DE AUDIO COM REDUÇÃO DE RUIDOS
    ADICIONANDO LINHA DE CODIGO NO ARQUIVO  default.pa  QUE IRA CRIAR UM CANAL DE REDUÇÃO DE RUIDO.

--- COMANDOS.sh
+++ COMANDOS.sh
@@ -1,4 +1,6 @@
-*** COMANDOS PARA RESOLUÇÃO DE PROBLEMAS OU DIFICULDADES EM DISTROS BASEADAS NO DEBIAN ***
+*** COMANDOS PARA RESOLUÇÃO DE PROBLEMAS OU DIFICULDADES EM DISTROS BASEADAS NO DEBIAN *** 
+		(COMANDOS PARA SEREM EXECUTADOS PELO TERMINAL)
+
 
 1 - HABILITAR O PAREAMENTO DO JOYSTICK (XBOX ONE) PARA USO VIA BLUETOOTH.
 
@@ -19,4 +21,8 @@ sudo apt-get install -y openvpn network-manager-openvpn network-manager-openvpn-
 05 - LIMPAR RESIDUOS DE CONFIGURAÇÃO PELO TERMINAL.
 
 sudo apt-get clean && sudo apt-get autoremove --purge -y $(dpkg -l | grep '^rc' | awk '{print $2}')
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+06 - HABILITAR CANAL DE AUDIO COM REDUÇÃO DE RUIDOS.
+nano /etc/pulse/default.pa (COMANDO QUE IRA ABRIR NO ARQUIVO DE CONFIGURAÇÃO PELO EDITOR "NANO")
+load-module module-echo-cancel (LINHA DE CODIGO QUE DEVE SER ADICIONADO NA PAGINA QUE IRA APARECER NO COMANDO ACIMA, SALVE A ALTERAÇÃO E REINICIE A MAQUINA)
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
